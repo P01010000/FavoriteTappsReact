1ac616b33514912e1529787af5daf60b9b0cb7ad
Heiner Pöpping <heiner.poepping@t-online.de>
2018-08-30 08:59:15 +0200

    Converted stateless components to constants

--- src/components/result/ResultListItem.jsx
+++ src/components/result/ResultListItem.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class ResultListItem extends React.Component {
-    static propTypes = {
-        appstoreName: PropTypes.string.isRequired,
-        siteId: PropTypes.string.isRequired,
-        locationId: PropTypes.number.isRequired
-    }
-
-    constructor() {
-        super();
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <div className="accordion__item">
-                <div className="ListItem ListItem--clickable" onClick={() => chayns.openUrlInBrowser(`https://chayns.net/${this.props.siteId}`)} onKeyPress={() => undefined} >
-                    <div className="ListItem__head">
-                        <div className="ListItem__Image" style={{ backgroundImage: `url(https://sub60.tobit.com/l/${this.props.locationId})`, backgroundSize: 'contain' }} />
-                        <div className="ListItem__Title">
-                            <p className="ListItem__Title--headline">{this.props.appstoreName}</p>
-                            <p className="ListItem__Title--description">{ this.props.siteId }</p>
-                        </div>
-                    </div>
+const ResultListItem = ({ appstoreName: name, siteId, locationId }) => (
+    <div className="accordion__item">
+        <div className="ListItem ListItem--clickable" onClick={() => chayns.openUrlInBrowser(`https://chayns.net/${siteId}`)} onKeyPress={() => undefined} >
+            <div className="ListItem__head">
+                <div className="ListItem__Image" style={{ backgroundImage: `url(https://sub60.tobit.com/l/${locationId})`, backgroundSize: 'contain' }} />
+                <div className="ListItem__Title">
+                    <p className="ListItem__Title--headline">{name}</p>
+                    <p className="ListItem__Title--description">{siteId}</p>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    </div>
+);
+
+ResultListItem.propTypes = {
+    appstoreName: PropTypes.string.isRequired,
+    siteId: PropTypes.string.isRequired,
+    locationId: PropTypes.number.isRequired
+};
 
 export default ResultListItem;
--- src/components/search/SearchContainer.jsx
+++ src/components/search/SearchContainer.jsx
@@ -14,7 +14,7 @@ class SearchContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tapps: [],
+      sites: [],
       reachedEnd: true,
       start: 0,
       take: ITEMS_PER_REQUEST,
@@ -41,7 +41,7 @@ class SearchContainer extends React.Component {
     try {
       const result = await fetchSites(searchString, start, take);
       this.setState({
-        tapps: append ? this.state.tapps.concat(result.sites) : result.sites,
+        sites: append ? this.state.sites.concat(result.sites) : result.sites,
         searchString,
         start: start + result.sites.length,
         reachedEnd: result.reachedEnd
@@ -55,7 +55,11 @@ class SearchContainer extends React.Component {
     return (
       <div className="accordion accordion--open" data-group="site" style={{ overflow: 'hidden', marginTop: '30px' }} >
         <SearchHead callback={this.loadNewData} />
-        <ResultList tapps={this.state.tapps} callback={this.loadMoreData} reachedEnd={this.state.reachedEnd} />
+        <ResultList
+          sites={this.state.sites}
+          callback={this.loadMoreData}
+          reachedEnd={this.state.reachedEnd}
+        />
       </div>
     );
   }
